46098738133076f69844779ea10042a528c86add
[Muahmmad Saadullah] <msaadullah2022@gmail.com>
2024-11-06 07:55:49 +0500

    fixed for vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-import Image from "next/image";
-import Link from "next/link";
-import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import Featuring from "./components/Featuring";
 
